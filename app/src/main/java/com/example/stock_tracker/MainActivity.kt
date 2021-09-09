09b4c114d58e0c8faba4b58cd9816c885aa772c7
JiajunAlan <alan233@my.yorku.ca>
2021-09-09 10:15:37 -0400

    optimize RecyclerView

--- app/src/main/java/com/example/stock_tracker/MainActivity.kt
+++ app/src/main/java/com/example/stock_tracker/MainActivity.kt
@@ -16,10 +16,8 @@ import com.example.stock_tracker.model.CompanyModel
 
 
 class MainActivity : AppCompatActivity() {
-    val url = "https://71iztxw7wh.execute-api.us-east-1.amazonaws.com/interview/favorite-stocks"
     lateinit var datalist : MutableList<CompanyModel>
     lateinit var recyclerviewAdapter: RecyclerViewAdapter
-    val context: Context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,49 +25,35 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
-        val btn_test = findViewById<Button>(R.id.btn_Test)
-        val btn_test2 = findViewById<Button>(R.id.btn_test2)
         val rv = findViewById<RecyclerView>(R.id.rv_list)
+        val data = CompanyData(this)
 
-        btn_test.setOnClickListener(){
-            val intent = Intent(this, Detail_Activity::class.java).apply {
-                val message :String = "Pega"
-                putExtra(EXTRA_MESSAGE, message)
-            }
-            startActivity(intent)
-            Toast.makeText(this,"go to next",Toast.LENGTH_SHORT).show()
-        }
-
-        btn_test2.setOnClickListener(){
-            val data = CompanyData(this)
-            data.getall(object : VolleyResponseListener {
-                override fun onSuccess(list: MutableList<CompanyModel>) {
-                    datalist = list
-
-                    rv.apply {
-                        layoutManager = LinearLayoutManager(context)
-                        recyclerviewAdapter = RecyclerViewAdapter(datalist)
-                        adapter = recyclerviewAdapter
-                    }
-
-
-
-                    Toast.makeText(context, list[0].toString(), Toast.LENGTH_SHORT ).show()
-                }
-
-                override fun onError(message: String) {
-                    Toast.makeText(context, "Error!", Toast.LENGTH_SHORT ).show()
-                }
-            })
-        }
-
-
-
-
+        updateList(data,rv)
 
     }
 
-
+    fun updateList(data:CompanyData,rv: RecyclerView){
+        data.getall(object : VolleyResponseListener {
+            override fun onSuccess(list: MutableList<CompanyModel>) {
+                datalist = list
+                rv.apply {
+                    layoutManager = LinearLayoutManager(this@MainActivity)
+                    recyclerviewAdapter = RecyclerViewAdapter(datalist)
+                    adapter = recyclerviewAdapter
+                }
+                //Toast.makeText(context, list[0].toString(), Toast.LENGTH_SHORT ).show()
+            }
+            override fun onError(message: String) {
+                Toast.makeText(this@MainActivity, "Error!", Toast.LENGTH_SHORT ).show()
+            }
+        })
+    }
 
 
 }
+//val intent = Intent(this, Detail_Activity::class.java).apply {
+//                val message :String = "Pega"
+//                putExtra(EXTRA_MESSAGE, message)
+//            }
+//            startActivity(intent)
+//            Toast.makeText(this,"go to next",Toast.LENGTH_SHORT).show()
